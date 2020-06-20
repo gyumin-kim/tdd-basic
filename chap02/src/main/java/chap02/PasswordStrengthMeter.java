d74ff18478b92a5ed46b64cb9a2aace229e0cd4a
Gyumin Kim <starkying@gmail.com>
2020-06-20 18:19:23 +0900

    (chap02) 3번째 테스트까지 추가

---
+++ chap02/src/main/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,23 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+	public PasswordStrength meter(final String s) {
+		if (s.length() < 8) {
+			return PasswordStrength.NORMAL;
+		}
+		boolean containsNum = meetsContainingNumberCriteria(s);
+		if (!containsNum)	return PasswordStrength.NORMAL;
+		return PasswordStrength.STRONG;
+	}
+
+	private boolean meetsContainingNumberCriteria(final String s) {
+		boolean containsNum = false;
+		for (char ch : s.toCharArray()) {
+			if (ch >= '0' && ch <= '9') {
+				containsNum = true;
+				break;
+			}
+		}
+		return containsNum;
+	}
+}
